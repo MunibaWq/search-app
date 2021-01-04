43ece814d60f15c202f9e76f302b8802ede648d2
Muniba Waqar <muniba.wq@gmail.com>
2021-01-04 11:03:42 -0700

    rendering the ImageCard.js component

--- src/components/imageList/ImageList.js
+++ src/components/imageList/ImageList.js
@@ -2,11 +2,12 @@ import React from "react";
 import ImageCard from "../imageCard/ImageCard";
 
 const ImageList = ({ images }) => {
-  console.log(images);
+  //   console.log(images);
 
-  images.map((image) => {
+  const imageList = images.map((image) => {
     return <ImageCard key={image.id} alt={image.description} src={image} />;
   });
+  return <div>{imageList}</div>;
 };
 
 export default ImageList;
