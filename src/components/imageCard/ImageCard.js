c406a031feb39c5c6755ca28c8c7639e8b7b3e8d
Muniba Waqar <muniba.wq@gmail.com>
2021-01-04 21:03:02 -0700

    adding spanValue

--- src/components/imageCard/ImageCard.js
+++ src/components/imageCard/ImageCard.js
@@ -3,22 +3,19 @@ import React, { useEffect, useRef, useState } from "react";
 const ImageCard = (props) => {
   const { description, urls } = props.image;
   const [span, setSpan] = useState(0);
-
-  //refs give access to a single DOM element
   const imageRef = useRef(0);
 
   useEffect(() => {
-    // console.log(`this is imageRef`);
-    // console.log(imageRef.current);
-    // console.log(imageRef.current.clientHeight);
     imageRef.current.addEventListener("load", setSpanValue);
   });
 
   const setSpanValue = () => {
-    console.log(imageRef.current.clientHeight);
+    const height = imageRef.current.clientHeight;
+    const spanValue = Math.ceil(height / 59);
+    setSpan(spanValue);
   };
   return (
-    <div className="image-card">
+    <div style={{ gridRowEnd: `span ${span}` }} className="image-card">
       <img ref={imageRef} alt={description} src={urls.regular} />
     </div>
   );
