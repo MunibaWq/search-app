f1eb9cef278005ade16d41742984dccb2a3de759
Muniba Waqar <muniba.wq@gmail.com>
2021-01-04 20:39:50 -0700

    adding eventListener

--- src/components/imageCard/ImageCard.js
+++ src/components/imageCard/ImageCard.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 
 const ImageCard = (props) => {
-  const [span, setSpan] = useState(0);
-  //refs give access to a single DOM element
-
-  const imageRef = useRef(0);
   const { description, urls } = props.image;
+  const [span, setSpan] = useState(0);
+
+  //refs give access to a single DOM element
+  const imageRef = useRef(0);
 
   useEffect(() => {
-    console.log(`this is imageRef`);
-    console.log(imageRef.current);
-    console.log(imageRef.current.clientHeight);
-    return () => {
-      const spans = imageRef.current.clientHeight;
-    };
+    // console.log(`this is imageRef`);
+    // console.log(imageRef.current);
+    // console.log(imageRef.current.clientHeight);
+    imageRef.current.addEventListener("load", setSpanValue);
   });
+
+  const setSpanValue = () => {
+    console.log(imageRef.current.clientHeight);
+  };
   return (
     <div className="image-card">
       <img ref={imageRef} alt={description} src={urls.regular} />
